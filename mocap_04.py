bd3c8e1e55d4e71527f22abbd70a955e707c8bef
Peter Berck <peter.berck@humlab.lu.se>
2022-12-14 15:34:24 +0100

    Better processing of annotations.

--- mocap_04.py
+++ mocap_04.py
@@ -215,7 +215,7 @@ group_RArm = ["x_RShoulderBack", "x_RShoulderTop", "x_RArm", "x_RElbowOut", "x_R
               "x_RHandOut", "x_RWristIn", "x_RWristOut" ]
 
 group_Body = ["x_BackL", "x_BackR", "x_Chest", "x_SpineTop", 
-              "x_WaistLBack", "x_WaistLFront", "x_WaistRBack", "x_WaistRFront"]
+              "x_WaistLBack", "x_WaistLFront", "X_WaistRBack", "x_WaistRFront"]
 
 
 # RESAMPLING
@@ -225,8 +225,8 @@ df_dists = df_dists.set_index(df_dists['td']) # and use it as index
 print( df_dists.head() )
 
 # max() works better than mean()
-#df_dists = df_dists.resample("50ms").max() # This resamples the 200 Hz to 20 Hz
-#print( df_dists.head() )
+df_dists = df_dists.resample("50ms").max() # This resamples the 200 Hz to 20 Hz
+print( df_dists.head() )
 
 # NAIVE IMPLEMENTATION OVER DISTANCE GROUPS
 
@@ -235,7 +235,9 @@ print( df_dists.head() )
 
 eaf = pympi.Elan.Eaf(file_path="mocap_valentijn/beach_repr_2.eaf", author='pympi')
 
-for sensor in ["x_LHandIn", "x_LHandOut", "x_LWristIn", "x_LWristOut"]:# group_LArm+group_RArm:
+for sensor in ["x_LHandIn", "x_LHandOut", "x_LWristIn", "x_LWristOut",
+               "x_RHandIn", "x_RHandOut", "x_RWristIn", "x_RWristOut",
+               "x_WaistLBack", "x_WaistRBack"]:# group_LArm+group_RArm:
     dist_max = df_dists[sensor].max()
     dist_min = df_dists[sensor].min()
     print( sensor, dist_min, dist_max )
@@ -249,12 +251,14 @@ for sensor in ["x_LHandIn", "x_LHandOut", "x_LWristIn", "x_LWristOut"]:# group_L
     previous_annotation = [0, 0]
     current_annotation = [0, 0]
     for ts, x in zip(df_dists["Timestamp"].values, df_dists[sensor].values):
+    #for ts, x in zip(df_dists.index.values, df_dists[sensor].values): # timedeltas are microseconds
         if not inside and x > threshold:
             print( "NEW {:.3f} {:.4f}".format(float(ts), float(x)) )
             inside = True
+            #st = int(ts / 1000000) # start time
             st = int(ts * 1000) # start time
             empty_time = st - previous_annotation[1] # to see if close to previous
-            if empty_time < 120: #arbitrary... 120ms
+            if empty_time < 200: #arbitrary... 120ms
                 print( "Short" )
                 print( " p ", previous_annotation )
                 print( " c ", current_annotation )
@@ -269,18 +273,20 @@ for sensor in ["x_LHandIn", "x_LHandOut", "x_LWristIn", "x_LWristOut"]:# group_L
         elif inside and x <= threshold:
             print( "--- {:.3f} {:.4f}".format(float(ts), float(x)) )
             inside = False
+            #et = int(ts / 1000000)
             et = int(ts * 1000)
             #eaf.add_annotation(sensor, st, et, value='Move')
             empty_time = et - previous_annotation[1] # not used
             # check for "too short"?
             previous_annotation = current_annotation
-            current_annotation += [et, empty_time] 
+            current_annotation += [et, empty_time]
             annotations.append( current_annotation )
             current_annotation = []
     # we might have lost the last one if it is "inside" until the end.
     print( annotations )
     for annotation in annotations:
-        eaf.add_annotation(sensor, annotation[0], annotation[1], value='Move')
+        if annotation[1] - annotation[0] > 250:
+            eaf.add_annotation(sensor, annotation[0], annotation[1], value='Move')
             
 eaf.to_file("mocap_valentijn/beach_repr_2_pb.eaf", pretty=True)
 
